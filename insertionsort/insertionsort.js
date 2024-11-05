e83b846518d623c91a363a689ff5f07b9fea95f6
Jon <lundbyjon@hotmail.com>
2024-11-05 18:02:35 +0100

    insertionSortShift complete

--- insertionsort/insertionsort.js
+++ insertionsort/insertionsort.js
@@ -1,4 +1,25 @@
-export function insertionSort(arr) {
-    // code...
-    console.log("insertionSort says hi!")
+export function insertionSortShift(arr) {
+    let iterations = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1; // holder styr på index før current
+
+        // "j >=0" sørger for at j-- ikke ender uden for minimum indeksering af array
+        // && så længe det der kom før current er større så bliver current sat til..
+        // ...det samme som kom før
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // efter j-- bliver current indsat på index pladsen før currents index plads
+        arr[j + 1] = current;
+
+        iterations++;
+    }
+
+    console.log(`InsertionShift, number of iterations: ${iterations}`);
+
+    return arr;
 }
--- insertionsort/test_insertionsort.js
+++ insertionsort/test_insertionsort.js
@@ -1,3 +1,9 @@
-import { insertionSort } from "./insertionsort.js";
+import { insertionSortShift } from "./insertionsort.js";
 
-insertionSort()
+const list = [50, 8, 2, 1, 0, 4, 3, 9, 7, 6];
+
+const arrTest = insertionSortShift(list)
+
+console.log(arrTest);
+
+
